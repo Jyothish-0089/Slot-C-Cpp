36254dc480e43ac2c151c3a37f3e38f8816b31b6
Jyothish <112233651+Jyothish-0089@users.noreply.github.com>
2022-09-01 11:57:23 +0530

    Add files via upload

---
+++ CLASSOBJ.CPP
@@ -0,0 +1,21 @@
+#include<iostream.h>
+class room{
+   public:
+      int l;
+      int b;
+
+      int area(){
+	 return 2*(l+b);
+      }
+      int volume(){
+	 return l*b;
+      }
+};
+int main(){
+   room r;
+   cin>>r.l;
+   cin>>r.b;
+   cout<<"Area :"<<r.area()<<endl;
+   cout<<"Volume :"<<r.volume()<<endl;
+   return 0;
+}
