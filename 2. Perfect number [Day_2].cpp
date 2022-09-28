b9ba56c92bc0573581c66d25029f4f0a9bc17046
Kaizer_Izumi <112233651+Jyothish-0089@users.noreply.github.com>
2022-09-28 05:56:30 +0200

    Add files via upload

---
+++ 2. Perfect number [Day_2].cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i,sum=0;
+	cout<<"Enter the number : ";
+	cin>>n;
+	cout<<"Divisors are :";
+	for(i=1;i<n;i++){
+		if(n%i==0){
+			cout<<i<<" ";
+			sum = sum + i;
+		}
+	}
+	cout<<"\nSum of divisors is : "<<sum<<"\n";
+	if(sum==n){
+		cout<<"So,"<<n<<" is a Perfect Number!";
+	}
+	else{
+		cout<<"So,"<<n<<" is not a Perfect Number..";
+	}
+	return 0;
+}
